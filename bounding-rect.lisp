bb2eaa26fa0b970ca775ebe62326b15996ca89b0
cel7t <shahsarthakw@gmail.com>
2023-01-09 14:50:06 +0530

    Added a README

--- bounding-rect.lisp
+++ bounding-rect.lisp
@@ -1,5 +1,5 @@
 #!/usr/bin/sbcl --script
-(load "/home/<USER>/.sbclrc")
+(load "/home/sixtyfour/.sbclrc")
 (ql:quickload :imago)
 
 (defpackage :bounding-rect
@@ -35,7 +35,7 @@
 		(list max-area max-rect (cons width height))))
 
 (defvar *img*
-	(imago:read-png "/home/<USER>/ProjectFolder/some_image.png"))
+	(imago:read-png "/home/sixtyfour/PJFOL/input.png"))
 
 (defvar *rect*
 	(bounding-rect *img*))
@@ -53,25 +53,16 @@
 			 nil))
 
 ;; was not made extern
-(defmethod compose-op-1 ((image imago:rgb-image))
+(defmethod compose-op-2 ((image rgb-image))
   (lambda (color1 color2)
-    (multiple-value-bind (a1 r1 g1 b1) (imago:color-argb color1)
-			(multiple-value-bind (a2 r2 g2 b2) (imago:color-argb color2)
-        (let* ((dominate (>= a1 a2))
-               (r (if dominate r1 r2))
-               (g (if dominate g1 g2))
-               (b (if dominate b1 b2)))
-          (imago:make-color r g b (max a1 a2)))))))
-
-(defmethod compose-op-2 ((image imago:rgb-image))
-  (lambda (color1 color2)
-    (multiple-value-bind (a1 r1 g1 b1) (imago:color-argb color1)
-			(multiple-value-bind (a2 r2 g2 b2) (imago:color-argb color2)
-        (let* ((dominate (>= a2 a1))
-               (r (if dominate r2 r1))
-               (g (if dominate g2 g1))
-               (b (if dominate b2 b1)))
-          (imago:make-color r g b (max a1 a2)))))))
+    (multiple-value-bind (a1 r1 g1 b1) (color-argb color1)
+      (multiple-value-bind (a2 r2 g2 b2) (color-argb color2)
+        (let* ((coef1 (if (>= a1 a2) (- a1 a2) 0))
+               (divisor (if (>= a1 a2) a1 a2))
+               (r (floor (+ (* coef1 r1) (* a2 r2)) divisor))
+               (g (floor (+ (* coef1 g1) (* a2 g2)) divisor))
+               (b (floor (+ (* coef1 b1) (* a2 b2)) divisor)))
+          (make-color r g b a1))))))
 
 (let* ((crop-area (cadr *rect*))
 			 (x (nth 0 crop-area))
@@ -80,39 +71,19 @@
 			 (h (nth 3 crop-area))
 			 (maxw (car (caddr *rect*)))
 			 (maxh (cdr (caddr *rect*)))
-			 (cropped
-				 (imago:crop *img* x y w h))
-			 (top-h y)
-			 (top ; x 0 to y, of width w
-				 (ifcrop *img* x 0 w top-h))
-			 (bottom-h (- maxh (+ y h)))
-			 (bottom ; x y+h to height, of width w
-				 (ifcrop *img* x (+ y h) w bottom-h))
-			 (left-w x)
-			 (left ; 0 to x, height h
-				 (ifcrop *img* 0 y left-w h))
-			 (right-w (- maxw (+ x w)))
-			 (right ; x+w to width, height h
-				 (ifcrop *img* (+ x w) y right-w h))
+			 ;; (cropped
+			 ;; 	 (imago:crop *img* x y w h))
 			 (new-img
-				 (make-instance (class-of cropped)
-												:width (imago:image-width cropped)
-												:height (imago:image-height cropped)))
-			 (dop (compose-op-1 left))
-			 (dop2 (compose-op-2 left)))
-	(if (not (null left))
-			(if (< left-w w)
-					(imago:copy new-img left :dest-x (- w left-w))
-					(imago:copy new-img left :src-x (- left-w w))))
-	(if (not (null bottom))
-			(if (< bottom-h h)
-					(imago:compose new-img new-img bottom 0 (- h bottom-h) dop)
-					(imago:compose new-img new-img bottom 0 (- bottom-h h) dop)))
-	(imago:compose new-img new-img cropped 0 0 dop2)
-	(if (not (null right))
-					(imago:compose new-img new-img right 0 0 dop2))
-	(if (not (null top))
-			(if (< bottom-h h)
-					(imago:compose new-img new-img top 0 (- h top-h) dop2)
-					(imago:compose new-img new-img top 0 (- top-h h) dop2)))
-	(imago:write-png new-img "/home/<USER>/ProjectFolder/result.png"))
+				 (make-instance (class-of *img*)
+												:width (+ x (* 2 w))
+												:height (+ y (* 2 h))))
+			 (dop2 (compose-op-2 new-img))
+			 (empty-pixel (imago:make-color 0 0 0 255)))
+	(imago:do-image-pixels (new-img colr itrx itry)
+		(setf colr empty-pixel))
+	(imago:compose new-img new-img *img* 0 0 dop2)
+	(imago:compose new-img new-img *img* 0 h dop2)
+	(imago:compose new-img new-img *img* w 0 dop2)
+	(imago:compose new-img new-img *img* w h dop2)
+	(setf new-img (imago:crop new-img (floor (/ w 2)) (floor (/ h 2)) w h))
+	(imago:write-png new-img "/home/sixtyfour/PJFOL/result.png"))
